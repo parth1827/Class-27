f3e9dc7972bed417bd6458a6b73a8e3fc73b621f
parth1827 <patelparth1827@gmail.com>
2020-11-18 15:55:50 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+var chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,6 +19,7 @@ function setup(){
     world = engine.world;
 
 
+    constrainedLog=new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -36,6 +40,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    chain1 = new Chain(bird.body,constrainedLog.body);
 }
 
 function draw(){
@@ -61,4 +66,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constrainedLog.display();
+    chain1.display();
 }
